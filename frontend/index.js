b040c01ad66402d594926a3812fb3bc9879b8e64
Akash Nigam <akashnigam2012@gmail.com>
2019-04-20 01:25:47 -0700

    added lineChart

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,10 @@
-BubbleChart.loadChart();
-$.getJSON("../data/sample_reduced_word_count.json",function(json) {
-    //word_count = json;
-    console.log(json); // this will show the info it in firebug console
-    WordCloud.drawWordCloud(json);
+$( document ).ready(function() {
+    console.log( "ready!" );
+    BubbleChart.loadChart();
+    $.getJSON("../data/sample_reduced_word_count.json",function(json) {
+        //word_count = json;
+        console.log(json); // this will show the info it in firebug console
+        WordCloud.drawWordCloud(json);
+    });
+    LineChart.createLine();
 });
